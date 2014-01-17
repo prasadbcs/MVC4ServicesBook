cb11f971dcbbb80d36caf49685dc8e8ae1ce1af4
prasadbcs <prasadbcs@gmail.com>
2014-01-17 14:46:26 -0500

    Initial Checkin

---
+++ src/MVC4ServicesBook.Web.Api.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC4ServicesBook.Web.Api.Models
+{
+    public class Class1
+    {
+    }
+}
